b3d28d81c9aaaa12e61487c2409aa46be92740e5
rakrachok <rakrachok@gmail.com>
2015-10-01 14:30:12 +0700

    Other-24459 Display users who make changes on the Admin DB Packages

---
+++ src/main/java/net/vqs/scmdb/dao/DbScriptDao.java
@@ -0,0 +1,95 @@
+package net.vqs.scmdb.dao;
+
+import net.vqs.scmdb.vo.DbScriptVo;
+import net.vqs.scmdb.vo.DbScriptStatus;
+import net.vqs.scmdb.vo.DbScriptType;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.annotation.Resource;
+
+@Component
+public class DbScriptDao {
+    private final String DELETE_BY_IDS = "delete from db_script where db_script_id in (:p_ids)";
+    private final String CREATE = "insert into db_script (name, file_hash, text, ts, output, type, status) values (?,?,?,?,?,?,?)";
+    private final String READ_ALL = "select * from db_script";
+    private final String READ_NEWEST = "select * from (select * from db_script order by name desc, ts desc) where rownum = 1";
+
+    @Resource
+    JdbcTemplate jdbcTemplate;
+
+    @Resource
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private RowMapper<DbScriptVo> rowMapper = new RowMapper<DbScriptVo>() {
+        public DbScriptVo mapRow(ResultSet rs, int i) throws SQLException {
+            DbScriptVo dbScript = new DbScriptVo();
+            dbScript.setDbScriptId(rs.getLong("DB_SCRIPT_ID"));
+            dbScript.setName(rs.getString("NAME"));
+            dbScript.setFileHash(rs.getString("FILE_HASH"));
+            dbScript.setText(rs.getString("TEXT"));
+            dbScript.setTs(rs.getDate("TS"));
+            dbScript.setOutput(rs.getString("OUTPUT"));
+            dbScript.setType(DbScriptType.getForInt(rs.getInt("TYPE")));
+            dbScript.setStatus(DbScriptStatus.getForInt(rs.getInt("STATUS")));
+            return dbScript;
+        }
+    };
+
+    private ResultSetExtractor<List<DbScriptVo>> dbScriptsExtractor = new ResultSetExtractor<List<DbScriptVo>>() {
+        public List<DbScriptVo> extractData(ResultSet rs) throws SQLException, DataAccessException {
+            List<DbScriptVo> dbScripts = new ArrayList<DbScriptVo>();
+            while (rs.next()) {
+                DbScriptVo dbScript = rowMapper.mapRow(rs, rs.getRow());
+                dbScripts.add(dbScript);
+            }
+            return dbScripts;
+        }
+    };
+
+    public List<DbScriptVo> readAll() {
+        return jdbcTemplate.query(READ_ALL, dbScriptsExtractor);
+    }
+
+    public DbScriptVo readNewest() {
+        return jdbcTemplate.queryForObject(READ_NEWEST, rowMapper);
+    }
+
+    public void batchCreate(final List<DbScriptVo> dbScripts) {
+        jdbcTemplate.batchUpdate(CREATE, new BatchPreparedStatementSetter() {
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                ps.setString(1, dbScripts.get(i).getName());
+                ps.setString(2, dbScripts.get(i).getFileHash());
+                ps.setString(3, dbScripts.get(i).getText());
+                ps.setDate(4, new Date(dbScripts.get(i).getTs().getTime()));
+                ps.setString(5, dbScripts.get(i).getOutput());
+                ps.setInt(6, dbScripts.get(i).getType().getTypeId());
+                ps.setInt(7, dbScripts.get(i).getStatus().getStatusId());
+            }
+
+            public int getBatchSize() {
+                return dbScripts.size();
+            }
+        });
+
+    }
+
+    public void deleteByIds(Collection<Long> delIds) {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("p_ids", delIds);
+        namedParameterJdbcTemplate.update(DELETE_BY_IDS, params);
+    }
+}
---
+++ src/main/java/net/vqs/scmdb/facade/CheckoutHelper.java
@@ -0,0 +1,157 @@
+package net.vqs.scmdb.facade;
+
+import net.vqs.scmdb.dao.DbScriptDao;
+import net.vqs.scmdb.vo.DbScriptStatus;
+import net.vqs.scmdb.vo.DbScriptType;
+import net.vqs.scmdb.vo.DbScriptVo;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.filefilter.AgeFileFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import javax.annotation.Resource;
+
+@Component
+public class CheckoutHelper {
+    @Resource
+    private DbScriptDao dbScriptDao;
+
+    private final String ROLLBACK_SUFX = "rollback";
+
+    private final String EXEC_FOLDER_NAME = "EXECUTE_ME";
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public void createAllFromPath(File scriptDir) {
+        Collection<File> files = FileUtils.listFiles(scriptDir, new String[]{"sql"}, false);
+        List<DbScriptVo> dbScripts = createAll(files);
+        dbScriptDao.batchCreate(dbScripts);
+    }
+
+    @Transactional
+    public List<File> checkoutDbFromPath(File scriptDir, List<DbScriptVo> dbScripts) {
+        DbScriptVo dbScriptVo = dbScriptDao.readNewest();
+        logger.debug("Searching new scripts in [{}]", scriptDir.getAbsolutePath());
+        Collection<File> files = FileUtils.listFiles(scriptDir, new AgeFileFilter(dbScriptVo.getTs(), false), null);
+        List<DbScriptVo> newDbScripts = createAll(files);
+        Iterator<DbScriptVo> iter = newDbScripts.iterator();
+        while (iter.hasNext()) {
+            DbScriptVo vo = iter.next();
+            if (dbScripts.contains(vo)) {
+                iter.remove();
+            }
+        }
+
+        logger.debug("Searching deleted scripts in [{}]", scriptDir.getAbsolutePath());
+        files = FileUtils.listFiles(scriptDir, new AgeFileFilter(dbScriptVo.getTs(), true), null);
+        List<DbScriptVo> delDbScripts = createAll(files);
+
+        File file = new File(scriptDir.getAbsolutePath() + File.separator + dbScriptVo.getName());
+        if (file.exists()) {
+            delDbScripts.add(dbScriptVo);
+        }
+
+        List<DbScriptVo> rollbackDbScripts = new ArrayList<DbScriptVo>();
+        List<Long> delIds = new ArrayList<Long>();
+        iter = dbScripts.iterator();
+        while (iter.hasNext()) {
+            DbScriptVo vo = iter.next();
+            if (!delDbScripts.contains(vo)) {
+                if (DbScriptType.ROLLBACK.equals(vo.getType())) {
+                    rollbackDbScripts.add(vo);
+                }
+                delIds.add(vo.getDbScriptId());
+            }
+        }
+
+        List<File> result = new ArrayList<File>();
+        for (DbScriptVo vo : rollbackDbScripts) {
+            File f = new File(scriptDir.getAbsolutePath() + File.separator + EXEC_FOLDER_NAME + File.separator
+                + vo.getName());
+            try {
+                logger.debug("Creating rollback script [{}]", f.getAbsolutePath());
+                FileUtils.writeStringToFile(f, vo.getText());
+                result.add(f);
+            } catch (IOException e) {
+                logger.error("Can't create file [{}]", f.getAbsolutePath(), e);
+                throw new RuntimeException(e);
+            }
+        }
+
+        for (DbScriptVo vo : newDbScripts) {
+            if (DbScriptType.ROLLBACK.equals(vo.getType())) {
+                continue;
+            }
+            File srcFile = new File(scriptDir.getAbsolutePath() + File.separator + vo.getName());
+            File destFile = new File(scriptDir.getAbsolutePath() + File.separator + EXEC_FOLDER_NAME
+                + File.separator + vo.getName());
+            try {
+                logger.debug("Copying new script [{}]", destFile.getAbsolutePath());
+                FileUtils.copyFile(srcFile, destFile);
+                result.add(destFile);
+            } catch (IOException e) {
+                logger.error("Can't copy file [{}]", srcFile.getAbsolutePath(), e);
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (!delIds.isEmpty()) {
+            logger.debug("Deleting missed scripts form db");
+            dbScriptDao.deleteByIds(delIds);
+        }
+
+        if (!newDbScripts.isEmpty()) {
+            logger.debug("Saving new scripts in db");
+            dbScriptDao.batchCreate(newDbScripts);
+        }
+
+        return result;
+    }
+
+    private List<DbScriptVo> createAll(Collection<File> files) {
+        Iterator<File> iter = files.iterator();
+        List<DbScriptVo> dbScripts = new ArrayList<DbScriptVo>();
+        while (iter.hasNext()) {
+            File file = iter.next();
+            DbScriptVo dbScriptVo = new DbScriptVo();
+            dbScriptVo.setName(file.getName());
+            try {
+                InputStream is = FileUtils.openInputStream(file);
+                String sha1Hex = DigestUtils.sha1Hex(is);
+                dbScriptVo.setFileHash(sha1Hex);
+                is.close();
+            } catch (IOException e) {
+                logger.warn("Can't generate hash for [{}]", file.getName(), e);
+            }
+            dbScriptVo.setTs(new Date(file.lastModified()));
+
+            String fileName = FilenameUtils.getBaseName(file.getName());
+            if (fileName.endsWith(ROLLBACK_SUFX)) {
+                try {
+                    String content = FileUtils.readFileToString(file);
+                    dbScriptVo.setText(content);
+                } catch (IOException e) {
+                    logger.warn("Can't read file [{}]", file.getName(), e);
+                }
+                dbScriptVo.setType(DbScriptType.ROLLBACK);
+            } else {
+                dbScriptVo.setType(DbScriptType.COMMIT);
+            }
+            dbScriptVo.setStatus(DbScriptStatus.EXECUTED);
+            dbScripts.add(dbScriptVo);
+        }
+        return dbScripts;
+    }
+}
