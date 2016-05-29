937d03699c4358901c69efc99d574172bd48c1ba
Dmitry Nesmelov <simplycissimys@gmail.com>
2016-05-29 20:17:10 +0700

    DB-31594 Add running sql scripts feature to scmdb fixed executing scripts when resources unavailable from jar file

--- src/main/java/com/onevizion/scmdb/SqlScriptExecutor.java
+++ src/main/java/com/onevizion/scmdb/SqlScriptExecutor.java
@@ -2,12 +2,14 @@ package com.onevizion.scmdb;
 
 import com.onevizion.scmdb.vo.DbCnnCredentials;
 import org.apache.commons.exec.*;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class SqlScriptExecutor {
     private static final Logger logger = LoggerFactory.getLogger(SqlScriptExecutor.class);
@@ -22,8 +24,6 @@ public class SqlScriptExecutor {
     }
 
     public int execute(File sqlScript) {
-        executor.setWorkingDirectory(sqlScript.getParentFile());
-
         CommandLine commandLine = new CommandLine(SQLPLUS_COMMAND);
         commandLine.addArgument("-L");
         commandLine.addArgument("-S");
@@ -34,10 +34,12 @@ public class SqlScriptExecutor {
             commandLine.addArgument(dbCnnCredentials.getOwnerCnnStr());
         }
 
-        File wrapperScriptFile = getWrapperScript(isUserSchemaScript);
+        File workingDir = sqlScript.getParentFile();
+        File wrapperScriptFile = getTmpWrapperScript(isUserSchemaScript, workingDir);
         commandLine.addArgument("@" + wrapperScriptFile.getAbsolutePath());
         commandLine.addArgument(sqlScript.getAbsolutePath());
 
+        executor.setWorkingDirectory(workingDir);
         try {
             return executor.execute(commandLine);
         } catch (ExecuteException e) {
@@ -46,19 +48,28 @@ public class SqlScriptExecutor {
         } catch (IOException e) {
             logger.error("Error during command execution", e);
             return 1;
+        } finally {
+            wrapperScriptFile.delete();
         }
     }
 
-    private File getWrapperScript(boolean isUserSchemaScript) {
+    private File getTmpWrapperScript(boolean isUserSchemaScript, File workingDir) {
         ClassLoader classLoader = getClass().getClassLoader();
-        File wrapperScript;
+        URL wrapperScript;
         if (isUserSchemaScript) {
-            wrapperScript = new File(classLoader.getResource("sqlplus_exit_code_wrapper.sql").getFile());
+            wrapperScript = classLoader.getResource("sqlplus_exit_code_wrapper.sql");
         } else {
-            wrapperScript = new File(classLoader.getResource("compile_invalids_wrapper.sql").getFile());
+            wrapperScript = classLoader.getResource("compile_invalids_wrapper.sql");
+        }
+        File tmpFile = new File(workingDir.getAbsolutePath() + File.pathSeparator + "tmp.sql");
+
+        try {
+            FileUtils.copyURLToFile(wrapperScript, tmpFile);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't copy tmp wrapper file.", e);
         }
 
-        return wrapperScript;
+        return tmpFile;
     }
 
     private boolean isUserSchemaScript(String scriptFileName) {
