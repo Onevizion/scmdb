b9f50442a9e731cc182770b31b473326b00aea2c
Rostislav Bespalov <rostbespalov@gmail.com>
2021-06-07 17:43:44 +0500

    Depl-177315 Package sqlcl into scmdb jar. Removing the formatting of the console execution log.

--- src/main/java/com/onevizion/scmdb/SqlScriptExecutor.java
+++ src/main/java/com/onevizion/scmdb/SqlScriptExecutor.java
@@ -4,18 +4,14 @@ import com.onevizion.scmdb.exception.ScriptExecException;
 import com.onevizion.scmdb.vo.DbCnnCredentials;
 import com.onevizion.scmdb.vo.SchemaType;
 import com.onevizion.scmdb.vo.SqlScript;
-import joptsimple.internal.Strings;
 import oracle.dbtools.raptor.newscriptrunner.ScriptExecutor;
 import oracle.dbtools.raptor.newscriptrunner.ScriptRunnerContext;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -25,7 +21,6 @@ import java.time.ZonedDateTime;
 import java.util.Date;
 
 import static com.onevizion.scmdb.ColorLogger.Color.GREEN;
-import static com.onevizion.scmdb.ColorLogger.Color.YELLOW;
 import static com.onevizion.scmdb.Scmdb.EXIT_CODE_SUCCESS;
 import static com.onevizion.scmdb.vo.SchemaType.OWNER;
 import static com.onevizion.scmdb.vo.ScriptType.COMMIT;
@@ -34,27 +29,17 @@ import static oracle.dbtools.raptor.newscriptrunner.ScriptRunnerContext.ERR_ENCO
 import static org.apache.commons.lang3.time.DurationFormatUtils.formatDurationHMS;
 
 public class SqlScriptExecutor {
-    private static final String SQL_CLIENT_COMMAND = "sql";
     private static final String SQL_COMMAND = "@%s %s";
 
-    private static final String INVALID_OBJECT_PREFIX = "Invalid objects in";
-    private static final String INVALID_OBJECT_REGEX = "^(\\w+\\s){0,2}\\w+\\s+\\S+\\s+is invalid.\\s*";
-    private static final String ERROR_STARTING_AT_LINE = "Error starting at line :";
     private static final String CREATE_SQL = "create.sql";
     private static final int SCRIPT_EXIT_CODE_ERROR = 1;
     private static final int SCRIPT_EXIT_CODE_SUCCESS = 0;
-    private static final String ERROR_LAST_LINE_START = "*Action:";
-
-    private boolean isErrorMsgStarted = false;
 
     @Autowired
     private AppArguments appArguments;
 
     @Autowired
     private ColorLogger logger;
-
-    public SqlScriptExecutor() {
-    }
 
     public int execute(SqlScript script) {
         DbCnnCredentials cnnCredentials = appArguments.getDbCredentials(script.getSchemaType());
@@ -64,9 +49,7 @@ public class SqlScriptExecutor {
         File workingDir = script.getFile().getParentFile();
         File wrapperScriptFile = getTmpWrapperScript(script.getSchemaType(), workingDir);
 
-        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
-             BufferedOutputStream bos = new BufferedOutputStream(baos);
-             Connection connection = DriverManager.getConnection(cnnCredentials.getOracleUrl(),
+        try (Connection connection = DriverManager.getConnection(cnnCredentials.getOracleUrl(),
                                                                  cnnCredentials.getSchemaName(),
                                                                  cnnCredentials.getPassword())) {
             connection.setAutoCommit(false);
@@ -74,24 +57,17 @@ public class SqlScriptExecutor {
             ScriptRunnerContext ctx = new ScriptRunnerContext();
 
             ctx.setBaseConnection(connection);
-            ctx.setOutputStreamWrapper(bos);
-            ctx.getOutputStream().setRemoveForcePrint(true);
 
             executor.setScriptRunnerContext(ctx);
-            executor.setOut(bos);
             executor.setStmt(String.format(SQL_COMMAND, wrapperScriptFile.getAbsolutePath(), script.getFile().getAbsolutePath()));
 
             Instant start = Instant.now();
             executor.run();
             String scriptExecutionTime = formatDurationHMS(Duration.between(start, Instant.now()).toMillis());
 
-            printFormattedScriptResult(baos.toString(StandardCharsets.UTF_8));
             logger.info("\n[{}] runtime: {}", GREEN, script.getName(), scriptExecutionTime);
 
             return (boolean) ctx.getProperty(ERR_ENCOUNTERED) ? SCRIPT_EXIT_CODE_ERROR : SCRIPT_EXIT_CODE_SUCCESS;
-        } catch (IOException e ) {
-            logger.error("Error.", e);
-            return SCRIPT_EXIT_CODE_ERROR;
         } catch (SQLException e) {
             logger.error("Error during connection DB.", e);
             return SCRIPT_EXIT_CODE_ERROR;
@@ -154,25 +130,4 @@ public class SqlScriptExecutor {
         }
     }
 
-    private void printFormattedScriptResult(String results) {
-        String[] lines = results.split("\n");
-
-        for (String line : lines) {
-            if (line.startsWith(INVALID_OBJECT_PREFIX)) {
-                logger.warn(line, YELLOW);
-            } else if (line.matches(INVALID_OBJECT_REGEX)) {
-                logger.warn(line, YELLOW);
-            } else if (line.startsWith(ERROR_STARTING_AT_LINE)) {
-                isErrorMsgStarted = true;
-                logger.error(line);
-            } else if (line.startsWith(ERROR_LAST_LINE_START) || Strings.isNullOrEmpty(line)) {
-                logger.error(line);
-                isErrorMsgStarted = false;
-            } else if (isErrorMsgStarted) {
-                logger.error(line);
-            } else {
-                logger.info(line);
-            }
-        }
-    }
 }
