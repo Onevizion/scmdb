9529485f272dfdb71bced8dbb8c66b4088f01343
Artyom Kazakov <artyom.kazakov@onevizion.com>
2025-01-17 10:42:22 +0500

    DB-244272 Unable to set values for index UTL_RECOMP_COMP_IDX1  DB-244272-186354 [CR] Unable to set values for index UTL_RECOMP_COMP_IDX1

--- src/main/resources/compile_schemas.sql
+++ src/main/resources/compile_schemas.sql
@@ -7,54 +7,74 @@ set FEEDBACK OFF
 declare
     -- The expected exception with the -20000 code is
     -- "Unable to set values for index UTL_RECOMP_COMP_IDX1: does not exist or insufficient privileges".
-    -- If it was thrown, it will wait 10 seconds and attempt to compile again.
-    c_expected_error_code number := -20000;
+    -- If it was thrown, it will wait 10 seconds and attempt to compile again, up to 3 times.
+    e_expected_recomp_exception exception;
+    pragma exception_init(e_expected_recomp_exception, c_expected_error_code);
+    c_expected_error_code constant number := -20000;
+    c_utl_recomp_index_name constant varchar2(20) := 'UTL_RECOMP_COMP_IDX1';
+    c_seconds_to_wait constant number := 10;
+    c_max_attempts constant number := 3;
+    v_attempt number;
     v_cnt   number;
 begin
 
-select count(*) into v_cnt from all_users where username = '&_USER._USER';
-if v_cnt = 1 then
-    begin
-        dbms_utility.compile_schema('&_USER._USER', false);
-    exception
-        when others then
-            if sqlcode = c_expected_error_code then
-                dbms_lock.sleep(10);
+    select count(*) into v_cnt from all_users where username = '&_USER._USER';
+    if v_cnt = 1 then
+        v_attempt := 0;
+        while true loop
+            begin
                 dbms_utility.compile_schema('&_USER._USER', false);
-            else
-                raise;
-            end if;
-    end;
-end if;
+                exit;
 
-select count(*) into v_cnt from all_users where username = '&_USER._RPT';
-if v_cnt = 1 then
-    begin
-        dbms_utility.compile_schema('&_USER._RPT', false);
-    exception
-        when others then
-            if sqlcode = c_expected_error_code then
-                dbms_lock.sleep(10);
+            exception
+                when e_expected_recomp_exception then
+                    v_attempt := v_attempt + 1;
+                    if instr(sqlerrm, c_utl_recomp_index_name) > 0 and v_attempt <= c_max_attempts then
+                        dbms_lock.sleep(c_seconds_to_wait);
+                    else
+                        raise;
+                    end if;
+            end;
+        end loop;
+    end if;
+
+    select count(*) into v_cnt from all_users where username = '&_USER._RPT';
+    if v_cnt = 1 then
+        v_attempt := 0;
+        while true loop
+            begin
                 dbms_utility.compile_schema('&_USER._RPT', false);
-            else
-                raise;
-            end if;
-    end;
-end if;
+                exit;
 
-select count(*) into v_cnt from all_users where username = '&_USER._PKG';
-if v_cnt = 1 then
-    begin
-        dbms_utility.compile_schema('&_USER._PKG', false);
-    exception
-        when others then
-            if sqlcode = c_expected_error_code then
-                dbms_lock.sleep(10);
+            exception
+                when e_expected_recomp_exception then
+                    v_attempt := v_attempt + 1;
+                    if instr(sqlerrm, c_utl_recomp_index_name) > 0 and v_attempt <= c_max_attempts then
+                        dbms_lock.sleep(c_seconds_to_wait);
+                    else
+                        raise;
+                    end if;
+            end;
+        end loop;
+    end if;
+
+    select count(*) into v_cnt from all_users where username = '&_USER._PKG';
+    if v_cnt = 1 then
+        v_attempt := 0;
+        while true loop
+            begin
                 dbms_utility.compile_schema('&_USER._PKG', false);
-            else
-                raise;
-            end if;
-    end;
-end if;
+                exit;
 
+            exception
+                when e_expected_recomp_exception then
+                    v_attempt := v_attempt + 1;
+                    if instr(sqlerrm, c_utl_recomp_index_name) > 0 and v_attempt <= c_max_attempts then
+                        dbms_lock.sleep(c_seconds_to_wait);
+                    else
+                        raise;
+                    end if;
+            end;
+        end loop;
+    end if;
 end;
