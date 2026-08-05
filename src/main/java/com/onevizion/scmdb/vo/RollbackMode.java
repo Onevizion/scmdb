881075addadd8edd969cd7f9c036341a03505901
Vasiliy Kupchinsky <vasiliy.kupchinskiy@onevizion.com>
2026-08-05 18:05:26 +0500

    Depl-272894 SCMDB: Implement support to automatic apply rollbacks mode Depl-272894-244212 [CR] SCMDB: Implement support to automatic apply rollbacks mode

--- src/main/java/com/onevizion/scmdb/vo/RollbackMode.java
+++ src/main/java/com/onevizion/scmdb/vo/RollbackMode.java
@@ -3,7 +3,9 @@ package com.onevizion.scmdb.vo;
 public enum RollbackMode {
 
     ASK /* Ask execute rollbacks or not */,
-    SKIP /* Do not execute rollback scripts */,
+    SKIP /* Do not execute rollback scripts and delete information about deleted scripts
+        (in compare with files in db/scripts directory) in DB_SCRIPT table, so next launch.
+        Previously this mode can be enabled by using --omit-changed command line argument */,
     FORCE_EXECUTE /* Execute rollback scripts without asking */
 
 }
