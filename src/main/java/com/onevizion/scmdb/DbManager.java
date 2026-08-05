f47946c7ee5589e95f8e166218fc0b06b9e49839
Vasiliy Kupchinsky <vasiliy.kupchinskiy@onevizion.com>
2026-08-05 18:13:08 +0500

    Depl-272894 SCMDB: Implement support to automatic apply rollbacks mode Depl-272894-244212 [CR] SCMDB: Implement support to automatic apply rollbacks mode

--- src/main/java/com/onevizion/scmdb/DbManager.java
+++ src/main/java/com/onevizion/scmdb/DbManager.java
@@ -143,6 +143,8 @@ public class DbManager {
     }
 
     private void checkDeletedScripts() {
+        logger.info("Rollback scripts execution mode: [{}]", appArguments.getRollbackMode());
+
         Map<String, SqlScript> deletedScripts = scriptsFacade.getDeletedScriptsMap();
         List<SqlScript> rollbacksToExec = deletedScripts.values()
                                                         .stream()
